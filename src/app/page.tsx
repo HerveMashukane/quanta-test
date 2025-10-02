ddb95fd735486f323d586d947c5af5b3cb6a1b19
Herve <hervemashukane@gmail.com>
2025-10-02 12:53:03 +0100

    add home welcoming page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main>
       <h1 className={styles.title}>
-        My Quanta Test on October 2, 2025
+        Hello, I am Herve Mashukane
       </h1>
+      <p>Welcome to My Quanta Test!</p>
     </main>
   )
 }
